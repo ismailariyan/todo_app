2c73414e9becccf0706bc92b61a5a48e209b4bca
Ismail Ariyan <ariyan.git.8hxlz@aleeas.com>
2023-03-27 01:48:54 +0600

    NICE

--- lib/db_handler.dart
+++ lib/db_handler.dart
@@ -40,6 +40,7 @@ class DBHelper {
         await _db!.rawQuery('select * from mytodo');
     return QueryResult.map((e) => TodoModel.fromMap(e)).toList();
   }
+
 //deleting data
   Future<int> delete(int id) async {
     var dbClient = await db;
